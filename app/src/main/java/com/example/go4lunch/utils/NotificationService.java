c3fddc31695830dfd163de93494afafc9fcc4dfb
aquaero7 <aquaero7@gmail.com>
2023-07-14 15:20:55 +0200

    NotificationService cleaning

--- app/src/main/java/com/example/go4lunch/utils/NotificationService.java
+++ app/src/main/java/com/example/go4lunch/utils/NotificationService.java
@@ -23,22 +23,7 @@ import java.util.List;
 
 public class NotificationService extends FirebaseMessagingService {
 
-    private final int NOTIFICATION_ID = 4;
-    // private final String NOTIFICATION_TAG = "GO4LUNCH";
-
-    private String currentDate = Utils.getCurrentDate();
-    private User currentUser;
-    private String currentUserId;
-    private List<User> workmatesList;
-    private List<String> selectorsNameList = new ArrayList<>();
-    private String selectionId;
-    private String selectionDate;
-    private String selectionName;
-    private String selectionAddress;
-    private String notificationPrefs;
-    private String notificationBodyText;
-    private UserRepository userRepository = UserRepository.getInstance();
-
+    private final UserRepository userRepository = UserRepository.getInstance();
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
@@ -47,69 +32,75 @@ public class NotificationService extends FirebaseMessagingService {
             // Get message sent by Firebase
             RemoteMessage.Notification notification = remoteMessage.getNotification();
             Log.i("NotificationService", notification.getTitle() + "\n" + notification.getBody());
-            // Get current user data
-            currentUser = userRepository.getCurrentUser();
-            currentUserId = currentUser.getUid();
-            selectionId = currentUser.getSelectionId();
-            selectionDate = currentUser.getSelectionDate();
-            selectionName = currentUser.getSelectionName();
-            selectionAddress = currentUser.getSelectionAddress();
-            notificationPrefs = currentUser.getNotificationsPrefs();
+            // Get current user
+            User currentUser = userRepository.getCurrentUser();
             // Check if current user has selected a restaurant
-            boolean aRestaurantIsSelected = selectionId != null && currentDate.equals(selectionDate);
-
+            boolean aRestaurantIsSelected = currentUser.getSelectionId() != null && Utils.getCurrentDate().equals(currentUser.getSelectionDate());
             /* Send notification with message only if current user has selected a restaurant
                and if his notifications preferences parameter is set true */
-            if (aRestaurantIsSelected && Boolean.parseBoolean(notificationPrefs)) {
+            if (aRestaurantIsSelected && Boolean.parseBoolean(currentUser.getNotificationsPrefs())) {
                 // Get workmates list
-                workmatesList = userRepository.getWorkmates();
+                List<User> workmatesList = userRepository.getWorkmates();
                 // Get the list of workmates names with the same selection (other than current user)
-                // Clear the selectors list of this restaurant
-                if (selectorsNameList != null) selectorsNameList.clear();
+                final List<String> selectorsNameList = new ArrayList<>();
                 // For each workmate, get his/her name and the selected restaurant id and date
                 for (User workmate : workmatesList) {
                     String wId = workmate.getUid();
                     // Get workmate selection only if he/she is not the current user
-                    if (!currentUserId.equals(wId)) {
+                    if (!currentUser.getUid().equals(wId)) {
                         String wName = workmate.getUsername();
                         String wSelId = (workmate.getSelectionId() != null) ? workmate.getSelectionId() : "";
                         String wSelDate = (workmate.getSelectionDate() != null) ? workmate.getSelectionDate() : "";
-                        /* If the workmate selection matches that of the current user,
-                           add his/her name to the selectors list */
 
-                        if (selectionId.equals(wSelId) && currentDate.equals(wSelDate)) selectorsNameList.add(wName);
+                        /* If the workmate selection matches that of the current user, add his/her name to the selectors list */
+                        if (currentUser.getSelectionId().equals(wSelId) && Utils.getCurrentDate().equals(wSelDate)) selectorsNameList.add(wName);
                     }
                 }
                 // Create notification body text
-                createNotificationBodyText(selectionName, selectionAddress, selectorsNameList);
+                String notificationBodyText = createNotificationBodyText(currentUser.getSelectionName(), currentUser.getSelectionAddress(), selectorsNameList);
+
                 // Send notification
-                sendLunchNotification();
+                sendLunchNotification(notificationBodyText);
             }
         }
     }
 
-    private void sendLunchNotification() {
+    private String createNotificationBodyText(String rName, String rAddress, List<String> selectorsNameList) {
+        String text = getString(R.string.notification_body_header) + "\n" + "\n" + rName + "\n" + rAddress;
+        if (selectorsNameList.isEmpty()) {
+            text = text + "\n" + "\n" + getString(R.string.text_none_joining);
+        } else {
+            text = text + "\n" + "\n" + getString(R.string.text_workmates_joining);
+            for (String selectorName : selectorsNameList) {
+                text = text + "\n- " + selectorName;
+            }
+        }
+        text = text + "\n" + "\n" + getString(R.string.text_greeting);
+        Log.i("NotificationService", text);
+
+        return text;
+    }
+
+    private void sendLunchNotification(String bodyText) {
+        final int NOTIFICATION_ID = 4;
+
         // Create an Intent that will be shown each day at 12 (configured in Firebase Cloud Messaging)
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-
         // Create a Channel (Android 8)
         String channelId = getString(R.string.default_notification_channel_id);
-
         // Build a Notification object
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
                         .setSmallIcon(R.drawable.ic_go4lunch)
                         .setContentTitle(getString(R.string.notification_title))
-                        // .setContentText(notificationBodyText)
-                        .setStyle(new NotificationCompat.BigTextStyle().bigText(notificationBodyText))
+                        // .setContentText(bodyText)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(bodyText))
                         .setAutoCancel(true)
                         .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                         .setContentIntent(pendingIntent);
-
         // Create the notification manager that will handle the notification
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
         // Create a new channel for Android versions >= 8
         CharSequence channelName = getString(R.string.notification_channel_name);   // "Firebase Messages";
         int importance = NotificationManager.IMPORTANCE_HIGH;
@@ -117,24 +108,7 @@ public class NotificationService extends FirebaseMessagingService {
         notificationManager.createNotificationChannel(mChannel);
 
         // Show notification
-        // notificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, notificationBuilder.build());
         notificationManager.notify(getString(R.string.app_name), NOTIFICATION_ID, notificationBuilder.build());
     }
-
-
-    private void createNotificationBodyText(String rName, String rAddress, List<String> selectorsNameList) {
-        notificationBodyText = getString(R.string.notification_body_header) + "\n" + "\n" + rName + "\n" + rAddress;
-        if (selectorsNameList.isEmpty()) {
-            notificationBodyText = notificationBodyText + "\n" + "\n" + getString(R.string.text_none_joining);
-        } else {
-            notificationBodyText = notificationBodyText + "\n" + "\n" + getString(R.string.text_workmates_joining);
-            for (String selectorName : selectorsNameList) {
-                notificationBodyText = notificationBodyText + "\n- " + selectorName;
-            }
-        }
-        notificationBodyText = notificationBodyText + "\n" + "\n" + getString(R.string.text_greeting);
-        Log.i("NotificationService", notificationBodyText);
-    }
-
 
 }
